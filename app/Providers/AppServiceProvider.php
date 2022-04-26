2b26c5baf3b90af1b58a7a9597498c1773e61526
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2022-04-26 16:38:12 -0300

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //Public folder name changed with public_html
+        $this->app->bind('path.public', function () {
+            return base_path() . '/html';
+        });
     }
 
     /**
